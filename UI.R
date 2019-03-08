abb09c1a66f9808fb482d51aa27f1e5c26d1fb95
ongels <megjcs@gmail.com>
2019-03-08 19:43:55 +0000

    Heading font and background colour

--- UI.R
+++ UI.R
@@ -1,7 +1,30 @@
 fluidPage(
-  title = "Words in Shakespeare's plays",
+  
+  theme = "shake_style.css",
+  
+  title = "Shakespearean words",
+  
+  tags$head(
+    tags$style(HTML("
+      @import url('//fonts.googleapis.com/css?family=Dancing%20Script');
+      
+      h1 {
+        font-family: 'Dancing Script';
+        font-weight: 500;
+        line-height: 1.5;
+        color: #7b694b;
+      }
 
-  textInput("focus_word", "Which word do you want to know about?"),
+    "))
+  ),
+          
+  headerPanel(h1("Words in Shakespeare's plays", align = "center")),
+  br(),
+
+  fluidRow(column(12, align = "center",
+    textInput("focus_word", "Which word do you want to know about?")
+  )
+    ),
   hr(),
   textOutput("word_total"),
   textOutput("word_frequency"),
