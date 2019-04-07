1742365fe12c3fd74677c2f3f7194d91824ac480
Megan Stodel <megjcs@gmail.com>
2019-04-07 16:04:11 +0100

    Remove all styling

--- UI.R
+++ UI.R
@@ -1,35 +1,24 @@
 fluidPage(
   
-  theme = "shake_style.css",
-  
   title = "Shakespearean words",
   
-  tags$head(
-    tags$style(HTML("
-                    @import url('//fonts.googleapis.com/css?family=Dancing%20Script');
-                    @import url('//fonts.googleapis.com/css?family=Charm');
-                    @import url('//fonts.googleapis.com/css?family=Lato');
-                    
-                    "))
-    ),
-  
-  headerPanel(h1("Words in Shakespeare's plays", align = "center")),
+  headerPanel("Words in Shakespeare's plays"),
   br(),
   
-  fluidRow(column(12, align = "center",
+  fluidRow(column(12, 
                   textInput("focus_word", "Which word do you want to know about?")
                   )
            ),
   hr(),
-  fluidRow(column(12, align = "center",
-                  htmlOutput("word_total"),
-                  htmlOutput("word_frequency"),
-                  htmlOutput("word_playtype")
+  fluidRow(column(12, 
+                  textOutput("word_total"),
+                  textOutput("word_frequency"),
+                  textOutput("word_playtype")
                   )
            ),
   hr(),
-  fluidRow(column(12, align = "center",
-                  htmlOutput("random_line")
+  fluidRow(column(12, 
+                  textOutput("random_line")
   )
   )
   
--- server.R
+++ server.R
@@ -11,12 +11,11 @@ function(input, output, session) {
   
   # Total number of times the word is used
   
-  output$word_total <- renderUI({
+  output$word_total <- renderText({
     req(input$focus_word != "")
-    HTML(paste0("<div id='mydiv'>", "Shakespeare used the word \"", input$focus_word, "\" ",
-           "<f><font size = 5><b>", 
-           prettyNum(shake_words[word %in% focus_word(), .N], big.mark = ","), "</f></font></b>",
-           ifelse(shake_words[word %in% focus_word(), .N] == 1, " time", " times") , " in his plays.", "</div>")
+    paste0("Shakespeare used the word \"", input$focus_word, "\" ",
+           prettyNum(shake_words[word %in% focus_word(), .N], big.mark = ","),
+           ifelse(shake_words[word %in% focus_word(), .N] == 1, " time", " times") , " in his plays."
     )
   })
 
@@ -27,21 +26,20 @@ top_plays <- reactive({
   return(top_plays)
 })
 
-output$word_frequency <- renderUI({
+output$word_frequency <- renderText({
   req(input$focus_word != "")
   req(focus_word() %in% shake_words$word)
   if(length(top_plays()) == 1) {
-    HTML(paste0("<div id='mydiv'>", "The play that uses the word \"", input$focus_word, "\" most often is ",
-                "<f><font size = 5><b>", top_plays(), "</f></font></b>", ", which uses it ", 
-                "<f><font size = 5><b>", prettyNum(word_in_plays()[, focus_word_freq][1], big.mark = ","), 
-                "</f></font></b>", ifelse(word_in_plays()[, focus_word_freq][1] == 1, " time.", " times.") , "</div")
+    paste0("The play that uses the word \"", input$focus_word, "\" most often is ",
+                top_plays(), ", which uses it ", 
+                prettyNum(word_in_plays()[, focus_word_freq][1], big.mark = ","), 
+                ifelse(word_in_plays()[, focus_word_freq][1] == 1, " time.", " times.")
     )
   } else {
-    HTML(paste0("<div id='mydiv'>", "The plays that use the word \"", input$focus_word, "\" most often are ",
-                "<f><font size = 5><b>", paste0(top_plays(), collapse = "</f></font></b> & <f><font size = 5><b>"), 
-                "</f></font></b>", ", which use it ", 
-                "<f><font size = 5><b>", prettyNum(word_in_plays()[, focus_word_freq][1], big.mark = ","), 
-                "</f></font></b>", ifelse(word_in_plays()[, focus_word_freq][1] == 1, " time.", " times.") , "</div")
+    paste0("The plays that use the word \"", input$focus_word, "\" most often are ",
+                paste0(top_plays(), collapse = " & "), ", which use it ", 
+                prettyNum(word_in_plays()[, focus_word_freq][1], big.mark = ","), 
+                ifelse(word_in_plays()[, focus_word_freq][1] == 1, " time.", " times.")
     )
   }
 })
@@ -54,30 +52,30 @@ focus_type <- reactive({
                                                               ifelse(title %in% tragedies, "Tragedies", "Problem plays")
                                                        )
   )]
+  word_in_type[, total := sum(focus_word_freq), by = type]
   word_in_type <- merge(word_in_type, all_type_words, by = "type")
   word_in_type[, proportion := total/V1]
-  word_in_type[proportion == max(proportion), type][1]
-  return(word_in_type$type)
+  word_in_type <- word_in_type[proportion == max(proportion), type][1]
+  return(word_in_type)
 })
 
 
-output$word_playtype <- renderUI({
+output$word_playtype <- renderText({
   req(input$focus_word != "")
   req(focus_word() %in% shake_words$word)
-  HTML(paste0("<div id='mydiv'>", "<f><font size = 5><b>", focus_type(), 
-              "</f></font></b>", " most often use the word \"", input$focus_word, "\", relative to their total number of words.")
-  )
+  paste0(focus_type(), " most often use the word \"", input$focus_word, "\", 
+         relative to their total number of words.")
 })
 
 # Generate a random line using word
 
-output$random_line <- renderUI({
+output$random_line <- renderText({
   req(input$focus_word != "")
   req(focus_word() %in% shake_words$word)
   sentences_with_word <- copy(shake_sentence)[grepl(paste0("\\b", input$focus_word, "\\b"), sentence, ignore.case = T)]
   select_sentence <- sentences_with_word[sample(nrow(sentences_with_word), 1)]
   select_sentence[, sentence := gsub("^[a-z]", toupper(substring(sentence, 1, 1)), sentence)]
-  random_sentence <- HTML(paste0("\"", select_sentence[, sentence], "\"<br /><font size = 4> - ", select_sentence[, title]))
+  random_sentence <- paste0("\"", select_sentence[, sentence], "\" - ", select_sentence[, title])
   return(random_sentence)
 })
 
