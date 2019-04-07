4d3785f5ecdb6183ca47e8d4b8b3f86798e87397
Megan Stodel <megjcs@gmail.com>
2019-04-07 17:44:42 +0100

    Added heading levels and alignment

--- UI.R
+++ UI.R
@@ -2,22 +2,22 @@ fluidPage(
   
   title = "Shakespearean words",
   
-  headerPanel("Words in Shakespeare's plays"),
+  headerPanel(h1("Words in Shakespeare's plays", align = "center")),
   br(),
   
-  fluidRow(column(12, 
+  fluidRow(column(12, align = "center",
                   textInput("focus_word", "Which word do you want to know about?")
                   )
            ),
   hr(),
-  fluidRow(column(12, 
+  fluidRow(column(12, align = "center",
                   textOutput("word_total"),
                   textOutput("word_frequency"),
                   textOutput("word_playtype")
                   )
            ),
   hr(),
-  fluidRow(column(12, 
+  fluidRow(column(12, align = "center",
                   textOutput("random_line")
   )
   )
