dc139231abd53ded17a384c1881fffe8ab33e085
Megan Stodel <megjcs@gmail.com>
2019-04-07 18:23:25 +0100

    Add CSS

--- UI.R
+++ UI.R
@@ -1,25 +1,39 @@
 fluidPage(
   
+  theme = "shake_style.css",
+  
   title = "Shakespearean words",
+  
+  tags$head(
+    tags$style(HTML("
+                    @import url('//fonts.googleapis.com/css?family=Dancing%20Script');
+                    @import url('//fonts.googleapis.com/css?family=Charm');
+                    @import url('//fonts.googleapis.com/css?family=Lato');
+                    
+                    "))
+    ),
   
   headerPanel(h1("Words in Shakespeare's plays", align = "center")),
   br(),
   
-  fluidRow(column(12, align = "center",
+  fluidRow(
+    column(12, align = "center",
                   textInput("focus_word", "Which word do you want to know about?")
-                  )
-           ),
+    )
+  ),
   hr(),
-  fluidRow(column(12, align = "center",
+  fluidRow(
+    column(12, align = "center",
                   textOutput("word_total"),
                   textOutput("word_frequency"),
                   textOutput("word_playtype")
-                  )
-           ),
+    )
+  ),
   hr(),
-  fluidRow(column(12, align = "center",
+  fluidRow(
+    column(12, align = "center",
                   textOutput("random_line")
-  )
+    )
   )
   
-    )
+)
