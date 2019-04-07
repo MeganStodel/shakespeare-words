9f4e487d82e911b91c47416d12c8488365eb3754
Megan Stodel <megjcs@gmail.com>
2019-04-07 17:47:12 +0100

    Remove columns to simplify

--- UI.R
+++ UI.R
@@ -4,22 +4,11 @@ fluidPage(
   
   headerPanel("Words in Shakespeare's plays"),
   br(),
-  
-  fluidRow(column(12, 
-                  textInput("focus_word", "Which word do you want to know about?")
-                  )
-           ),
+  textInput("focus_word", "Which word do you want to know about?"),
   hr(),
-  fluidRow(column(12, 
-                  textOutput("word_total"),
-                  textOutput("word_frequency"),
-                  textOutput("word_playtype")
-                  )
-           ),
+  textOutput("word_total"),
+  textOutput("word_frequency"),
+  textOutput("word_playtype"),
   hr(),
-  fluidRow(column(12, 
-                  textOutput("random_line")
-  )
-  )
-  
+  textOutput("random_line")
     )
